554aedc2bbf2568b37fe7d186426e1231a2a57d1
Aman Sikarwar <amansik.1910@gmail.com>
2023-01-27 22:03:32 +0530

    initialized the repository

---
+++ example/moodle_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:moodle_api/moodle_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/moodle_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library moodle_api;
+
+export 'src/moodle_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
